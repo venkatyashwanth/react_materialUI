3799aa29928601c1a438089001852415269737c2
venkat yashwanth <mudiliyashwanth@gmail.com>
2022-06-09 18:01:06 +0530

    material ui demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import { Button, styled } from "@mui/material";
+import AdbIcon from '@mui/icons-material/Adb';
+import AddIcon from '@mui/icons-material/Add';
+const App = () => {
+  const BlueButton = styled(Button)(({theme})=>({
+    backgroundColor: theme.palette.otherColor.main,
+    color: "#888",
+    margin: 5,
+    "&:hover":{
+      backgroundColor:"green",
+      color: "white"
+    }
+  }))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button startIcon={<AdbIcon />} variant="contained" color="success">
+        Success
+      </Button>
+      <Button startIcon={<AddIcon />} variant="contained" color="secondary">
+        Secondary
+      </Button>
+
+      <BlueButton>My Button</BlueButton>
+
+
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette:{
+        primary:{
+            main: "#17605",
+            light: "skyblue"
+        },
+        secondary:{
+            main: "#00FF00"
+        },
+        otherColor: {
+            main: "#999"
+        }
+    }
+})
